0b001c264f2d61ca453515ac55b8f93d1e5f5e86
jatinthelord <jatinthelord@gmail.com>
2026-04-16 14:28:30 +0530

    Create RabbitBrowser.bash

---
+++ RabbitBrowser.bash
@@ -0,0 +1,10 @@
+# Rabbit browser
+
+cd rabbit-browser
+git add .
+git commit -m "🐰 Rabbit Browser"
+
+# GitHub pe push karo
+git remote add origin https://github.com/jatinthelord/rabbit-browser.git
+git branch -M main
+git push -u origin main
